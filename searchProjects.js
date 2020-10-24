f1cd932d3acb309cde5d7ad2e8856e9ac75e7f5d
adamrturman <adamrturman@gmail.com>
2020-10-24 14:13:25 -0500

    solved fizzbuzz

---
+++ searchProjects.js
@@ -0,0 +1,3 @@
+// 1: Write a function that searches collection "projects" to find the object with the lowest "e" value and return that object's "i" value.
+// getLowestValue([{i:10, e: 100},{i:20, e: 200}]); // returns 10
+var projects = [{i:1, e:41}, {i:5, e:4}, {i:3, e:2}, {i:1, e:90}, {i:5, e:40}, {i:3, e:20}];
