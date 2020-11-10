ce8f33ad62f84cd64fc1e17bae9ed1a517ee05bb
adamrturman <adamrturman@gmail.com>
2020-11-10 16:55:24 -0600

    refactored jewels and stones to use a set

--- solved/jewelsAndStones.js
+++ solved/jewelsAndStones.js
@@ -19,22 +19,39 @@
 
 //  This is a brute force solution O (n^2) - Need to refactor to use a set
 
-const numJewelsInStones = function(J, S) {
-    //  start a count
+// const numJewelsInStones = function(J, S) {
+//     //  start a count
+//     let count = 0;
+//     //  split J into array of characters, those are the jewels we are looking for
+//     const arrayOfJewels= J.split('');
+//     //  split S into array of characters
+//     const arrayOfStones= S.split('');
+//     //  loop through both arrays
+//     for (let i=0; i<arrayOfJewels.length; i++) {
+//         for (let k=0; k<arrayOfStones.length; k++) {
+//             // if we find an element in arrayOfStones that is also in arrayOfJewels
+//             if (arrayOfJewels[i] === arrayOfStones[k]) {
+//                 // increment the count
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// };
+
+//  Refactored to use a set
+
+const numJewelsInStones = function (J, S) {
     let count = 0;
-    //  split J into array of characters, those are the jewels we are looking for
-    const arrayOfJewels= J.split('');
-    //  split S into array of characters
-    const arrayOfStones= S.split('');
-    //  loop through both arrays
-    for (let i=0; i<arrayOfJewels.length; i++) {
-        for (let k=0; k<arrayOfStones.length; k++) {
-            // if we find an element in arrayOfStones that is also in arrayOfJewels
-            if (arrayOfJewels[i] === arrayOfStones[k]) {
-                // increment the count
-                count++;
-            }
+    const stoneArray = S.split('');
+    const jewelSet = new Set(J);
+    //  for every element in the stoneArray that is also in the jewelSet, increment the count
+    stoneArray.forEach(element => {
+        if (jewelSet.has(element)) {
+            count++;
         }
-    }
-    return count;
+    })
+    return count
 };
+
+console.log(numJewelsInStones("aA", "aAAbbbb"))
