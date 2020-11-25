1c8f2918ff809ce05f2f98c9d03f4b3470962f9a
adamrturman <adamrturman@gmail.com>
2020-11-25 14:03:50 -0600

    refactored findDuplicate to include the edge case of no duplicates in array

--- findDuplicate.js
+++ findDuplicate.js
@@ -14,8 +14,21 @@
 
 
 
+// const findFirstDuplicate = function(nums) {
+//     return nums.reduce(function (accumulator, currentValue) {
+//         if (accumulator.numbersSeenAlready.has(currentValue)) {
+//             accumulator.processedNums.push(currentValue);
+//         }
+//         accumulator.numbersSeenAlready.add(currentValue);
+//         return accumulator;
+//     }, {
+//         numbersSeenAlready: new Set(),
+//         processedNums: []
+//     }).processedNums[0];
+// };
+
 const findFirstDuplicate = function(nums) {
-    return nums.reduce(function (accumulator, currentValue) {
+    const answer = nums.reduce(function (accumulator, currentValue) {
         if (accumulator.numbersSeenAlready.has(currentValue)) {
             accumulator.processedNums.push(currentValue);
         }
@@ -24,8 +37,16 @@ const findFirstDuplicate = function(nums) {
     }, {
         numbersSeenAlready: new Set(),
         processedNums: []
-    }).processedNums[0];
+    });
+    if (answer.processedNums.length === 0) {
+        return false;
+    } else {
+        return answer.processedNums[0];
+    }
 };
+console.log(findFirstDuplicate([2,1,3,5,3,2])) //   returns 3
+console.log(findFirstDuplicate([2,1,3,5,3,2])) //   returns 3
+console.log(findFirstDuplicate([1,2,3,4,5]))    //  returns false
 
 // const findFirstDuplicate = function(nums) {
 //     let finalAccumulator = nums.reduce(function (accumulator, currentValue) {
