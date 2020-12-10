74792ce968a50352074534c138a2b37a3a4e96d3
adamrturman <adamrturman@gmail.com>
2020-12-09 19:09:41 -0600

    included one line solution for checkTwoStringArrays using join method

--- solved/checkTwoStringArrays.js
+++ solved/checkTwoStringArrays.js
@@ -10,11 +10,8 @@
 // word1 represents string "ab" + "c" -> "abc"
 // word2 represents string "a" + "bc" -> "abc"
 // The strings are the same, so return true.
-// Example 2:
-// Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
-// Output: false
 
-const arrayStringsAreEqual = function(word1, word2) {
+const arrayStringsAreEqual1 = function(word1, word2) {
     const combinedWord1 = word1.toString().replace(/,/gm, '' );
     const combinedWord2 = word2.toString().replace(/,/gm, '' );
     if (combinedWord1 === combinedWord2) {
@@ -23,5 +20,25 @@ const arrayStringsAreEqual = function(word1, word2) {
     return false;
 };
 
-console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"])) //  returns true
-console.log(arrayStringsAreEqual(["a", "cb"], ["a", "bc"])) //  returns false
+const arrayStringsAreEqual2 = function(word1, word2) {
+    function smush(part1, part2) {
+        return part1 + part2;
+    }
+    return word1.reduce(smush) === word2.reduce(smush)
+};
+
+const arrayStringsAreEqual3 = function(word1, word2) {
+    function combine (...words) {
+        return words.toString().replace(/,/gm, '')
+    }
+    return combine(word1) === combine(word2)
+};
+
+const arrayStringsAreEqual4 = function(word1, word2) {
+    return word1.join('') === word2.join('')
+}
+
+console.log(arrayStringsAreEqual1(["ab", "c"], ["a", "bc"])) //  returns true
+console.log(arrayStringsAreEqual2(["a", "cb"], ["a", "bc"])) //  returns false
+console.log(arrayStringsAreEqual3(["ab", "c"], ["a", "bc"])) //  returns true
+console.log(arrayStringsAreEqual3(["abc", "d", "defg"],["abcddefg"]))   //  returns true
