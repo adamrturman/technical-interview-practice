6aba54a95236dc2023e7c72e2c2f156a79c9fa6a
adamrturman <adamrturman@gmail.com>
2021-02-04 10:24:03 -0600

    solved halvesAreAlike with slice method and regex

---
+++ solved/havlesAreAlike.js
@@ -0,0 +1,25 @@
+const halvesAreAlike = function(s) {
+    //  slice the input in half
+    const firstHalf = s.slice(0, s.length/2);
+    const secondHalf = s.slice(s.length/2);
+
+    //  use a regex to create an array of vowels
+    function vowelCount(string) {
+        const vowelArray = string.match(/[AEIOUaeiou]/gm)
+        //  if we find any vowels, return the length of that array
+        if (vowelArray) {
+            return vowelArray.length;
+        }
+        //  if we don't find any vowels, return 0
+        return 0;
+    }
+    //  compare the two counts of vowels
+    return vowelCount(firstHalf) === vowelCount(secondHalf);
+};
+
+console.log(halvesAreAlike('textbook')) //  returns false
+console.log(halvesAreAlike('MerryChristmas')) //  returns false
+console.log(halvesAreAlike('book')) //  returns true
+console.log(halvesAreAlike('AbCdEfGh')) //  returns true
+console.log(halvesAreAlike('qwrtyplkjhgfdszxcvbnm'))    //  returns true
+
